17d798e946e36b82018332e7c161a06c90cbeb24
simple710 <112814424+simple0710@users.noreply.github.com>
2022-09-12 21:12:00 +0900

    silver_5

--- silver/[1475] 방 번호.py
+++ silver/[1475] 방 번호.py
@@ -5,7 +5,7 @@ for i in range(len(n)):
     num = int(n[i])
     # 만약 6이나 9일 경우
     if num == 6 or num == 9:
-        # 하나의 수만 증가하는 것이 아닌 각각의 수를 올
+        # 하나의 수만 증가하는 것이 아닌 각각의 수를 올림
         if data[6] <= data[9]:
             data[6] += 1
         else:

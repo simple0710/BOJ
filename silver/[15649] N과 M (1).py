33d23179af6540f2d4db7f3253bbb2a641dc10b7
simple710 <112814424+simple0710@users.noreply.github.com>
2022-09-21 23:50:49 +0900

    silver_3

---
+++ silver/[15649] N과 M (1).py
@@ -0,0 +1,15 @@
+def dfs():
+  if len(s) == m:
+    print(' '.join(map(str,s)))
+    return
+  # 트리 구조를 생각한다.
+  ## 1(pop) - (2,3,4) 각각 pop()
+  ## 2(pop) - (1,2,3) 각각 pop()
+  for i in range(1, n+1):
+    if i not in s:
+      s.append(i)
+      dfs()
+      s.pop()  
+n, m = map(int, input().split())
+s = list()
+dfs()

8ceca8394bafe66bf1fb4e915416ed851780eb83
simple710 <112814424+simple0710@users.noreply.github.com>
2022-09-25 22:00:29 +0900

    silver_1

--- silver/[2583] 영역 구하기(DFS).py
+++ silver/[2583] 영역 구하기(DFS).py
@@ -1,10 +1,15 @@
+import sys
+sys.setrecursionlimit(10**6)
+
 def dfs(x, y):
   global cnt
   dx = [-1, 1, 0, 0]
   dy = [0, 0, -1, 1]
+  # 네 방향을 탐지한다
   for i in range(4):
     nx = x + dx[i]
     ny = y + dy[i]
+    # 만약 범위 내, 0인 경우 cnt += 1을 하고 해당 구역을 1로 하고 재귀를 한다.
     if 0 <= nx < m and 0 <= ny < n and data[nx][ny] == 0:
       data[nx][ny] = 1
       cnt += 1
@@ -21,14 +26,17 @@ for _ in range(k):
 
 area = 0
 cnt_data = list()
+# 전 지역을 탐색한다.
 for x in range(m):
   for y in range(n):
+    # 만약 직사각형 구역이 아닌 경우
     if data[x][y] == 0:
       data[x][y] = 1
-      area += 1
+      area += 1 # 구역에 대해 1 증가
       cnt = 1
-      dfs(x, y)
-      cnt_data.append(cnt)
+      dfs(x, y) # 구역의 수를 반환
+      cnt_data.append(cnt) # 리스트에 추가
 cnt_data.sort()
+
 print(area)
 print(' '.join(map(str, cnt_data)))

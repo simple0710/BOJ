6b0525954ead13e3a77129db7750df33eafbdc01
simple710 <112814424+simple0710@users.noreply.github.com>
2022-10-28 12:00:48 +0900

    silver_4

--- silver/[2331] 반복수열.py
+++ silver/[2331] 반복수열.py
@@ -1,12 +1,16 @@
+# 2022/10/28
+
+# 정보 입력
 A, P = map(int,input().split())
 arr = [A]
 while True:
   sum = 0
   for i in str(arr[-1]):
     sum += int(i) ** P
+  # 반복이 시작됐을 경우
   if sum in arr:
     break
   arr.append(sum)
-  
 
+# 출력  
 print(arr.index(sum))

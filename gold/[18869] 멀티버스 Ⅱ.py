825c87cee2d9416715506a3a6684ec824c8c5195
simple710 <112814424+simple0710@users.noreply.github.com>
2023-01-07 21:32:24 +0900

    gold_5

---
+++ gold/[18869] 멀티버스 Ⅱ.py
@@ -0,0 +1,18 @@
+# 2022/11/19 정렬, 값/좌표 압축
+import sys
+input = sys.stdin.readline
+from collections import defaultdict
+
+m, n = map(int, input().split())
+universe = defaultdict(int)
+for _ in range(m):
+    planets = list(map(int, input().split()))
+    keys = sorted(list(set(planets)))
+    ranks = {keys[i]: i for i in range(len(keys))}
+    add = tuple([ranks[x] for x in planets])
+    universe[add] += 1
+
+ans = 0
+for cnt in universe.values():
+    ans += cnt * (cnt - 1) // 2
+print(ans)

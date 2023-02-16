386abcb4864f9befab45a76706c26db897a14fa1
simple710 <112814424+simple0710@users.noreply.github.com>
2023-02-16 20:26:41 +0900

    gold_2

--- Gold/[8980] 택배.py
+++ Gold/[8980] 택배.py
@@ -13,10 +13,10 @@ my_box = [C] * (N+1)
 res = 0
 for s, e, b in box:
   check = C
-  for i in range(s, e): # e에 도착하기 전에 들고 있는 박스의 값 저장
+  for i in range(s, e): # e에 도착하기 전에 들고 있을 수 있는 박스의 개수 저장
     check = min(check, my_box[i])
-  check = min(check, b) # 들고 있을 수 있는 박스 양중 가장 작은 값을 check로 한다
-  for i in range(s, e): # 목적지까지 갈 때 해당 공간에서 가지고 있을 수 있는 박스의 값을 구한다.
+  check = min(check, b) # 들고 있을 수 있는 박스의 개수와 현재 박스의 개수 중 가장 작은 값을 check로 한다
+  for i in range(s, e): # 목적지까지 도착할 때까지 해당 공간에서 가지고 있을 수 있는 박스의 값을 구한다.
     my_box[i] -= check
   
   # 배송한 값 더하기

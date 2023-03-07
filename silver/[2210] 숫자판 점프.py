1125f033643438653d6968f7a38c93a48d0f35f1
simple710 <thesimpleman@naver.com>
2023-03-07 09:53:51 +0900

    silver_2

---
+++ silver/[2210] 숫자판 점프.py
@@ -0,0 +1,31 @@
+# 2023/03/07 dfs
+# https://www.acmicpc.net/problem/2210
+import sys
+input = sys.stdin.readline
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+
+def dfs(x, y, depth, word):
+  # 길이가 5인 경우
+  if depth == 5:
+    global res
+    # res에 추가하고 종료
+    res.add(word)
+    return
+  
+  for i in range(4): # 4방향을 돌아본다.
+    nx = x + dx[i]
+    ny = y + dy[i]
+    if 0 <= nx < 5 and 0 <= ny < 5:
+      nword = data[nx][ny] + word
+      dfs(nx, ny, depth+1, nword)
+
+data = [list(map(str,input().rstrip().split())) for _ in range(5)]
+res = set()
+for i in range(5):
+  for j in range(5):
+    # 위치, 깊이, 단어
+    dfs(i, j, 0, data[i][j])
+
+# 정답 출력
+print(len(res))

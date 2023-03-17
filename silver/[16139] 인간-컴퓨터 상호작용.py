0ae27b24432f5ae1c5a5a9d9ffde496ed3cc617d
simple710 <thesimpleman@naver.com>
2023-03-17 12:48:41 +0900

    silver_1

---
+++ silver/[16139] 인간-컴퓨터 상호작용.py
@@ -0,0 +1,16 @@
+# 2023/03/17 Prefix Sum
+# https://www.acmicpc.net/problem/16139
+import sys
+input = sys.stdin.readline
+
+word = input().rstrip()
+dic = {}
+dic[-1] = {chr(i):0 for i in range(ord('a'), ord('z')+1)}
+for i in range(len(word)): # 누적 합 계산
+  dic[i] = dic[i-1].copy()
+  dic[i][word[i]] += 1
+
+for _ in range(int(input())):
+  q = input().split()
+  # 정답 출력
+  print(dic[int(q[2])][q[0]]- dic[int(q[1])-1][q[0]])

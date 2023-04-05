2c480aac499cda96cf9006d84c87a33ee0275975
simple710 <thesimpleman@naver.com>
2023-04-05 09:36:24 +0900

    silver_3

---
+++ silver/[7795] 먹을 것인가 먹힐 것인가.py
@@ -0,0 +1,18 @@
+# 2023/04/05 Sorting, TwoPointer
+# https://www.acmicpc.net/problem/7795
+for _ in range(int(input())):
+  N, M = map(int,input().split())
+  A = sorted(list(map(int,input().split())), reverse = True)
+  B = sorted(list(map(int,input().split())))
+  res = 0
+  e = M - 1
+  for i in A:
+    while e >= 0:
+      if B[e] < i: # 해당 값보다 큰 경우
+        # e + 1 만큼의 쌍이 생성된다.
+        res += e + 1
+        break
+      else:
+        e -= 1
+  # 정답 출력
+  print(res)

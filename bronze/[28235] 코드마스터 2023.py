b7507348e6698f8bcb938d01fd30fb97dc288740
simple0710 <thesimpleman@naver.com>
2023-09-12 20:39:20 +0900

    bronze_5

---
+++ bronze/[28235] 코드마스터 2023.py
@@ -0,0 +1,12 @@
+# 2023/06/17 Implemnetation
+# https://www.acmicpc.net/problem/28235
+string = input()
+# 응원에 맞는 구호를 출력한다.
+if string == 'SONGDO':
+  print("HIGHSCHOOL")
+elif string == 'CODE':
+  print("MASTER")
+elif string == "2023":
+  print("0611")
+else:
+  print("CONTEST")

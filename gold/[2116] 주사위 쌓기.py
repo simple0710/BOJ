aefe342f03ea1145c1682b522509b8d3b9db42ac
simple0710 <thesimpleman@naver.com>
2023-09-28 11:13:28 +0900

    gold_5

--- gold/[2116] 주사위 쌓기.py
+++ gold/[2116] 주사위 쌓기.py
@@ -10,7 +10,7 @@ def solution(N, dice):
   # A - F, B - D, C - E
   tb_dict = { 0 : 5, 1 : 3, 2 : 4, 4 : 2, 3 : 1, 5 : 0}
   res = 0
-  for i in range(6):
+  for i in range(6): # 시작은 자유롭게 놓을 수 있다.
     top = dice[0][i]
     cnt = value_get(top, dice[0][tb_dict[i]])
     for j in range(1, N):
@@ -19,7 +19,7 @@ def solution(N, dice):
       top = dice[j][tb_dict[dice[j].index(bottom)]]
       cnt += value_get(top, bottom) # 옆면의 최댓값 더하기
     res = max(res, cnt) # 값 갱신
-  return res
+  return res # 정답 반환
 
 def main():
   N = int(input())

56867253c25bd771703a9e180c0bd0ec745829e7
simple0710 <thesimpleman@naver.com>
2023-10-11 11:08:40 +0900

    silver_5

--- silver/[25757] 임스와 함께하는 미니게임.py
+++ silver/[25757] 임스와 함께하는 미니게임.py
@@ -4,7 +4,7 @@ import sys
 input = sys.stdin.readline
 
 def solution(N, G, P):
-   # 임스는 중복된 사람과 다시 플레이 하지 않는다.
+   # 임스는 중복된 사람과 게임을 다시 플레이 하지 않는다.
    # 따라서 중복되지 않은 사람 // 게임의 플레이어 수가 성립한다.
   tp = {'Y' : 1, 'F' : 2, 'O' : 3}
   return len(set(P)) // tp[G] # 정답 반환
